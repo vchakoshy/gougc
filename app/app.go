9c83bf7be48e46918c353d4f4339b5ffd669843e
vchakoshy <vchakoshy@gmail.com>
2024-06-05 15:11:12 +0330

    add app

---
+++ app/app.go
@@ -0,0 +1,47 @@
+package app
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	"github.com/vchakoshy/gougc/service/auth"
+	"github.com/vchakoshy/gougc/service/user"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+type App struct {
+	db     *gorm.DB
+	router *gin.Engine
+}
+
+func NewApp() App {
+	// docker run --name go-postgres --rm -p 5432:5432 -e POSTGRES_PASSWORD=123456 -d postgres
+	dsn := "user=postgres password=123456 host=localhost dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Tehran"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
+		SingularTable: false,
+	}})
+	if err != nil {
+		log.Fatal("gorm.Open error ", err)
+	}
+	return App{
+		db:     db,
+		router: gin.Default(),
+	}
+}
+
+func (a App) Run() {
+	v1 := a.router.Group("/api/v1")
+	{
+		user.NewModule(a.db, v1)
+		auth.NewModule(a.db, v1)
+	}
+
+	a.router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	a.router.Run()
+
+}
--- cmd/api.go
+++ cmd/api.go
@@ -1,13 +1,11 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+	"github.com/vchakoshy/gougc/app"
 )
 
 // apiCmd represents the api command
@@ -21,7 +19,8 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("api called")
+		a := app.NewApp()
+		a.Run()
 	},
 }
 
